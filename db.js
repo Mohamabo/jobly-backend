a04b34602ff2d654b94a5d34eab5af209ed1b377
Mohamabo <m.abouelkhair91@gmail.com>
2025-08-09 16:00:34 -0400

    db change

--- db.js
+++ db.js
@@ -13,21 +13,9 @@ if (process.env.NODE_ENV === "production") {
     },
   });
 } else {
-  // For development, use connection object instead of connection string
-  // if DATABASE_URL is not a full URL
-  const dbUri = getDatabaseUri();
-  if (dbUri.startsWith("postgres://") || dbUri.startsWith("postgresql://")) {
-    db = new Client({
-      connectionString: dbUri,
-    });
-  } else {
-    // Treat as database name only
-    db = new Client({
-      database: dbUri,
-      host: "localhost",
-      port: 5432,
-    });
-  }
+  db = new Client({
+    connectionString: getDatabaseUri(),
+  });
 }
 
 db.connect();
